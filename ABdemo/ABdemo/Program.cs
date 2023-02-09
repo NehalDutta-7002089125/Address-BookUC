f955478ec23d2066ba76ca9349a33b48c2dfe5ee
“NehalDutta-7002089125” <nihal326dutta@gmail.com>
2023-02-09 19:35:04 +0530

    [NehalDutta]Add: create the contact no

---
+++ ABdemo/ABdemo/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ABdemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("welcome to AddressBook");
+            Person person = new Person();
+            while (true)
+            {
+                Console.WriteLine("Welcome to Address Book Program");
+                Console.WriteLine("Select Option:\n" +
+                    "1) Find the Create Contacts\n");
+
+                int option = Convert.ToInt32(Console.ReadLine());
+
+                switch (option)
+                {
+                    case 1:
+                        Person.CreateContacts();
+                        break;
+                    default:
+                        Console.WriteLine("Choose valid option");
+                        break;
+                }
+            }
+        }
+    }
+}
