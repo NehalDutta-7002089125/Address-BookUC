bb9db1e55356fb5959d07db11692a22bbc55e95c
“NehalDutta-7002089125” <nihal326dutta@gmail.com>
2023-02-09 19:58:06 +0530

    [NehalDutta]Add: adding multiple the contact no

--- ABdemo/ABdemo/Program.cs
+++ ABdemo/ABdemo/Program.cs
@@ -6,36 +6,51 @@ namespace ABdemo
     {
         static void Main(string[] args)
         {
-
             Console.WriteLine("welcome to AddressBook");
             Person person = new Person();
             while (true)
             {
                 Console.WriteLine("Welcome to Address Book Program");
                 Console.WriteLine("Select Option:\n" +
-                    "1) Find the Create Contacts\n");
-
-                var option = Convert.ToInt32(Console.ReadLine());
-
+                    "1) Find the Create Contacts\n" +
+                    "2) Find the Display of Contacts\n" +
+                    "3) Find the edit contacts\n" +
+                    "4) Find the delete contacts\n" +
+                    "5) Ability to add Multiple person in AddressBook" +
+                     "6) Enter Correct number");
+                int option = Convert.ToInt32(Console.ReadLine());
+                int count = 0;
                 switch (option)
                 {
                     case 1:
                         Person.CreateContacts();
                         break;
+
                     case 2:
                         Person.Display();
                         break;
+
                     case 3:
                         Person.editContact();
                         break;
+
                     case 4:
                         Console.WriteLine("Enter name to delete the record");
                         string name = Console.ReadLine();
                         Person.DeleteContact(name);
                         break;
+
+                    case 5:
+                        Person.AddMultipleContacts();
+                        break;
+
+
                     default:
                         Console.WriteLine("Choose valid option");
                         break;
+
+
+
                 }
             }
         }
